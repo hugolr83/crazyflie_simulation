baecb70b2a0b8ec6436868b5187f0855954ba946
Wail <abderrahmane-wail.ayad@polymtl.ca>
2021-09-28 20:23:51 -0400

    WIP

---
+++ controllers/cf_controller/cf_controller.cpp
@@ -0,0 +1,5 @@
+#include <argos3/core/utility/configuration/argos_configuration.h>
+#include "cf_controller.h"
+
+
+REGISTER_CONTROLLER(CrazyflieController, "cf_controller")
---
+++ controllers/cf_controller/cf_controller.h
@@ -0,0 +1,8 @@
+#ifndef CRAZYFLIE_CONTROLLER_H
+#define CRAZYFLIE_CONTROLLER_H
+
+class CrazyflieController: public CCI_Controller{
+
+};
+
+#endif
